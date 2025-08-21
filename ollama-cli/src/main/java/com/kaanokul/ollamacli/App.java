00ee1f7efb5961b55f82ebaa6046e28e02fed4ab
kaanismet <kaanismet7@gmail.com>
2025-08-20 22:34:48 -0500

    create maven project

---
+++ ollama-cli/src/main/java/com/kaanokul/ollamacli/App.java
@@ -0,0 +1,13 @@
+package com.kaanokul.ollamacli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
